2d9aa297e551003383f544749015b666702d019e
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2023-06-07 16:23:56 -0300

    adicionado o schema de autores

--- src/roteador.js
+++ src/roteador.js
@@ -1,12 +1,20 @@
 import express from "express";
 import LivroController from "./controladores/livros.js";
+import AutoresController from "./controladores/autores.js";
 
 const rotas = express()
 
-rotas.get("/", LivroController.listarLivros)
-rotas.get("/:id", LivroController.listarLivrosPorId)
+rotas.get("/livros", LivroController.listarLivros)
+rotas.get("/livros/:id", LivroController.listarLivrosPorId)
 rotas.post("/livros", LivroController.cadastrarLivro)
 rotas.put("/livros/:id", LivroController.atualizarLivro)
-rotas.delete("/:id", LivroController.excluirLivro)
+rotas.delete("/livros/:id", LivroController.excluirLivro)
+
+
+rotas.get("/autores", AutoresController.listarAutores)
+rotas.get("/autores/:id", AutoresController.listarAutoresPorId)
+rotas.post("/autores", AutoresController.cadastrarAutor)
+rotas.put("/autores/:id", AutoresController.atualizarAutor)
+rotas.delete("/autores/:id", AutoresController.excluirAutor)
 
 export default rotas
